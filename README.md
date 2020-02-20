d6c5312b6c6c644cd3e22052c16b14e905652534
cassandraortiz <cassy.ortiz5@gmail.com>
2020-02-20 02:22:57 -0800

    dynamic image set, fixing unique photos

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Welcome to Bus Mall!
+# Welcome to Bus Mall
 
 BusMall is a catalog of assorted high-markup products provided to a captive audience seeking a mental escape from the drudgery of travel on busses.
 
 BusMall wants to feature only the items in its catalog that are the most likely to sell.  Currently, this website is doing marketing analysis to test their potential customer items of interest.
 
-### **Gathering information**
+### *Gathering information*
 
 Each user will be asked to choose their favorite item from a series of randomly selected items.   Upon selecting your favorite the images will change, although some images may repeat after a *x-number* of rotations.
 
@@ -22,36 +22,36 @@ No installations currently needed.
 
 ### GitHub Repo |  [bus-mall](https://github.com/cassandraortiz/bus-mall)
 
-#### BRANCH: `Lab11-busmall` [(Requirements)](https://canvas.instructure.com/courses/1791697/assignments/13320251?return_to=https%3A%2F%2Fcanvas.instructure.com%2Fcalendar%23view_name%3Dmonth%26view_start%3D2020-02-19) 
+#### BRANCH: `Lab11-busmall` [(Requirements)](https://canvas.instructure.com/courses/1791697/assignments/13320251?return_to=https%3A%2F%2Fcanvas.instructure.com%2Fcalendar%23view_name%3Dmonth%26view_start%3D2020-02-19)
 
  3 Random Images - non repeating in consecutive view;   25 responses, then show list of viewed and votes for each image object.
 
- - Pull Request: [#1](https://github.com/cassandraortiz/bus-mall/pull/1) 
-   - Commits 
-     - finished requirements Lab11 \| [`d40136`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/40fdca01764870dd9faa1526b43c1e3c3bab6080)
-     - stretch goal: dynamic # of images \| [`ffda86`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/ffda867fd1ec77a16219399ffdd903afdfe2db9b)
-     - changed the paragraph \| [`40fdca`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/d401361a9bf80defaf2b55efea2ad397c0351f62)
-
-
+- Pull Request: [#1](https://github.com/cassandraortiz/bus-mall/pull/1)
+  - Commits 
+    - finished requirements Lab11 \| [`d40136`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/40fdca01764870dd9faa1526b43c1e3c3bab6080)
+    - stretch goal: dynamic # of images \| [`ffda86`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/ffda867fd1ec77a16219399ffdd903afdfe2db9b)
+    - changed the paragraph \| [`40fdca`](https://github.com/cassandraortiz/bus-mall/pull/1/commits/d401361a9bf80defaf2b55efea2ad397c0351f62)
 
 #### BRANCH: `chartjs` [(Requirements)](https://canvas.instructure.com/courses/1791697/assignments/13320252)
 
 Changed non-repeating image to 2 turns.  Render a bar chart after 25 votes have been obtained.
 
- - Pull Request: [#2](https://github.com/cassandraortiz/bus-mall/pull/2)
-   - Commits
-     - Updated Chart, applied function, added arrays - still some kinks \| `7cd58e`
-     - Added Random RGBA to my data - created all arrays \| `8004b9`
- - Pull Request: [#3/4](https://github.com/cassandraortiz/bus-mall/pull/3)
-   - Commits
-     - updated the README files \| [`000000`]()
-
+- Pull Request: [#2](https://github.com/cassandraortiz/bus-mall/pull/2)
+  - Commits
+    - Updated Chart, applied function, added arrays - still some kinks \| `7cd58e`
+    - Added Random RGBA to my data - created all arrays \| `8004b9`
+- Pull Request: [#3](https://github.com/cassandraortiz/bus-mall/pull/3)
+  - Commits
+    - updated the README files \| [`108beca`](https://github.com/cassandraortiz/bus-mall/pull/3/commits/108beca76c96a3d07d7f7c1868a5062df7f152b1) , [`ddd71b1`](https://github.com/cassandraortiz/bus-mall/pull/3/commits/ddd71b1b1ffddd1897bc8765ce6c53d24a42c7bd) , [`11cf527`](https://github.com/cassandraortiz/bus-mall/pull/3/commits/11cf5279114339efd30fceb138a433cd25ee3142)
+- Pull Request: [#4](https://github.com/cassandraortiz/bus-mall/pull/4)
+  - Commits
+    - doing something...
+  
 ---
 
 ## Collaborators
 
 Currently, code demos during Class 11 & 12 have helped guide working in this website.
-
 
 ---
 
@@ -60,6 +60,5 @@ Currently, code demos during Class 11 & 12 have helped guide working in this web
 [Cheat-Sheet JavaScript](http://overapi.com/javascript) \| reference sheet for array and Math methods.
 
 [Chart.js](https://www.chartjs.org/docs/latest/) \| Used for Bar chart
-
 
 ---
--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 'use strict';
 
-var img1 = document.getElementById('img1');
-var img2 = document.getElementById('img2');
-var img3 = document.getElementById('img3');
+// var img1 = document.getElementById('img1');
+// var img2 = document.getElementById('img2');
+// var img3 = document.getElementById('img3');
 var imgSection = document.getElementById('pictureSection');
 var resultList = document.getElementById('resultsList');
 
-var ImageList = ['bag','banana', 'bathroom','boots', 'breakfast', 'bubblegum', 'chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+
 var allItems = [];
-//var prevItem = [];
-var newArray = [];
+var prevArray = [];
 var labelArray = [];
 var viewsArray = [];
 var votesArray = [];
 var color1Array = [];
 var color2Array = [];
+
 var timesClicked = 0;
+var testImages = 3;
+var uniqueClicks = 2;
 
+//================================================
+// Setups the Number of pictures to be selected
+// - adds to section: pictureSection
+// - invokes function
+//------------------------
+function setupPictures(numberPictures){
+  for(var p = 1; p <= numberPictures; p++){
+    var testImage = document.createElement('img');
+    testImage.id = `img${p}`;
+    console.log(`img${p}`);
+    imgSection.appendChild(testImage);
+  }
+}
 
-// Object Function
+setupPictures(testImages);
+
+//=====================================
+// [Object]  ItemImage
+// -- holds the information about the Image
+//------------------------
 function ItemImage(src, alt, title){
   this.src = src;
   this.alt = alt;
@@ -29,71 +49,161 @@ function ItemImage(src, alt, title){
   allItems.push(this);
 }
 
+var ImageList = ['bag','banana', 'bathroom','boots', 'breakfast', 'bubblegum', 'chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 
-// randomizes the image index
-function randomImage(max){
+// Creates Objects 'ItemImage'
+// - Loops through ImageList array
+//----------------------------------
+for (var x = 0; x < ImageList.length; x++){
+  var imgName = ImageList[x];
+  var src = `img/${imgName}.jpg`;
+  new ItemImage(src, imgName, imgName);
+}
+
+//=====================================
+// RANDOM FUNCTIONS
+//---------------------
+function randomNumber(max){
   return Math.floor(Math.random() * max);
 }
 
-// setup the Images
-function setupImages(){
+function getRGBA(){
+  var r = randomNumber(255);
+  var b = randomNumber(255);
+  var g = randomNumber(255);
+  var a = Math.random();
 
-  // keep randomizing Images (until)
+  return `rgba(${r},${b},${g},${a})`;
+}
+//=====================================
+
+
+function displayImages(){
+  var displayArray = [];
+  var max = allItems.length;
+
+  
+
   do{
-    var pic1 = randomImage(allItems.length);
-    var pic2 = randomImage(allItems.length);
-    var pic3 = randomImage(allItems.length);
+    //displayArray = [];
+    
+    for(var pic = 0; pic < testImages; pic++){
+      var randoPic = randomNumber(max);
+  
+      while (prevArray.includes(randoPic)){
+        randoPic = randomNumber(max);
+      }
+      
+      if (displayArray.includes(randoPic)){
+        displayArray.splice(displayArray.indexOf(randoPic),1);
+      } else if (displayArray.length < testImages){
+        displayArray.push(randoPic);
+      } else {
+        displayArray.push(randoPic);
+      }
 
-    // if pics equal to each other -- random number is included in array
-    // - add viewed tally to the object
-  } while (pic1 === pic2 || pic2 === pic3 || pic1 === pic3 || newArray.includes(pic1) || newArray.includes(pic2) || newArray.includes(pic3));{
-    allItems[pic1].viewed++;
-    allItems[pic2].viewed++;
-    allItems[pic3].viewed++;
+    }
+    // for(var pic = 0; pic < testImages; pic++){
+    //   var randoPic = randomNumber(max);
+
+    //   if(displayArray.includes(randoPic)){
+    //     displayArray[displayArray.indexOf(randoPic)] = randomNumber(max);
+    //   } else if (displayArray.length === testImages-1){
+    //     displayArray.shift();
+    //     displayArray.push(randoPic);
+    //   } else {
+    //     displayArray.push(randoPic);
+    //   }
+    // }
+  } while (displayArray.length === testImages-1);{
+    console.log(`while: prevArray: ${prevArray}  Display array: ${displayArray}`);
   }
 
-  // reset the New array
-  // newArray = [];
+  for (var i = 1; i <= testImages; i++){
+    var imgID = `img${i}`;
+    var indexID = displayArray[i-1];
+    var imgElement = document.getElementById(imgID);
+    var uniqueImages = uniqueClicks * testImages;
 
-  // loops through each Image and applies the information
-  //alert(imgSection.childElementCount+1);
-
-  var numberImages = imgSection.childElementCount;
-
-  for (var i = 1; i < numberImages + 1; i++){
-    var objectName = `img${i}`;
-    var indexID = `pic${i}`;
-
-    // adds to newArray max 2 turns
-
-    var clicks = 2;
-    var picShown = clicks * numberImages;
-
-    if(newArray.length === picShown){
-      console.log('unshifting:');
-      newArray.shift();
-      newArray.push(eval(indexID));
+    if(prevArray.length === uniqueImages){
+      prevArray.shift();
+      prevArray.push(indexID);
     } else {
-      console.log('pushing:');
-      newArray.push(eval(indexID));
+      prevArray.push(indexID);
     }
 
-    eval(objectName).src = allItems[eval(indexID)].src;
-    eval(objectName).title = allItems[eval(indexID)].title;
-    eval(objectName).alt = allItems[eval(indexID)].alt;
+    // Applys image element requirements
+    imgElement.src = allItems[indexID].src;
+    imgElement.title = allItems[indexID].title;
+    imgElement.alt = allItems[indexID].alt;
+    allItems[indexID].viewed ++;
   }
 }
 
 
-// this sets up all my images - based on the array 'ImageList'
-for (var x = 0; x < ImageList.length; x++){
-  var name = ImageList[x];
-  var src = `img/${name}.jpg`;
-  new ItemImage(src, name, name);
-}
+displayImages();
 
-// Call initial Image Setup
-setupImages(allItems.length);
+
+// // setup the Images
+// function setupImages(){
+//   var showArray = [];
+//   // keep randomizing Images (until)
+//   do{
+
+//     for(var z = 0; z < testImages; z++){
+//       var number = randomNumber(allItems.length);
+//       if (!showArray.includes(number)){
+//         showArray[z] = number;
+//       }
+//     }
+//     // var pic1 = randomNumber(allItems.length);
+//     // var pic2 = randomNumber(allItems.length);
+//     // var pic3 = randomNumber(allItems.length);
+
+//     // if pics equal to each other -- random number is included in array
+//     // - add viewed tally to the object
+//   } while (newArray.includes(showArray)){
+//   //   for(var q = 0; q <= testImages; q++){
+//   //     allItems[showArray[q]].viewed ++;
+//   //   }
+//   // }
+//   // } while (pic1 === pic2 || pic2 === pic3 || pic1 === pic3 || newArray.includes(pic1) || newArray.includes(pic2) || newArray.includes(pic3));{
+//   //   allItems[pic1].viewed++;
+//   //   allItems[pic2].viewed++;
+//   //   allItems[pic3].viewed++;
+//   // }
+
+//   // loops through each Image and applies the information
+//   // var numberImages = imgSection.childElementCount;
+
+//   for (var i = 1; i <= testImages; i++){
+//     var objectName = `img${i}`;
+//     var indexID = `pic${i}`;
+
+//     var element = document.getElementById(objectName);
+//     var picShown = uniqueClicks * testImages;
+
+//       if(newArray.length === picShown){
+//         newArray.shift();
+//         newArray.push(eval(indexID));
+//       } else {
+//         newArray.push(eval(indexID));
+//       }
+
+//     element.src = allItems[eval(indexID)].src;
+//     element.title = allItems[eval(indexID)].title;
+//     element.alt = allItems[eval(indexID)].alt;
+//     allItems[eval(indexID)].viewed ++;
+
+
+//     // eval(objectName).src = allItems[eval(indexID)].src;
+//     // eval(objectName).title = allItems[eval(indexID)].title;
+//     // eval(objectName).alt = allItems[eval(indexID)].alt;
+//   }
+// }
+
+
+
 
 imgSection.addEventListener('click', handleclick);
 
@@ -108,7 +218,7 @@ function handleclick(e){
       }
     }
 
-    setupImages();
+    displayImages();
     timesClicked++;
 
   } else if (timesClicked === 25) {
@@ -118,6 +228,7 @@ function handleclick(e){
   }
 
 }
+
 
 
 function reportInfo(){
@@ -133,42 +244,19 @@ function reportInfo(){
   }
 }
 
-function randoColor(){
-  return Math.floor(Math.random() * 255);
-}
 
-function getRGBA(){
-  var r = randoColor();
-  var b = randoColor();
-  var g = randoColor();
-  var a = Math.random();
-
-  return `rgba(${r},${b},${g},${a})`;
-}
 
 
 function renderCanvas(){
-var ctx = document.getElementById('barChart');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-      labels: labelArray,
-      datasets: [{
-          label: '# of Clicks per View',
-          data: viewsArray,
-          backgroundColor: color1Array,
-          borderColor: color2Array,
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
-});
+  var ctx = document.getElementById('barChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {labels: labelArray,
+      datasets: [{label: '# of Clicks per View',
+        data: viewsArray,
+        backgroundColor: color1Array,
+        borderColor: color2Array,
+        borderWidth: 1}]},
+    options: {scales:{yAxes: [{ticks: {beginAtZero: true}}]}}
+  });
 }
